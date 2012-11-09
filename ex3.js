387c090de7a2a2b8b5dde0c5eccb234cc18558a4
Thibault <thibault.denizet@ingesup.com>
2012-11-09 12:22:23 +0100

    Delete disgusting indentation

--- ex3.js
+++ ex3.js
@@ -2,8 +2,8 @@ var http = require("http");
 var d1 = new Date().getTime();
 
 http.get("http://search.twitter.com/search.json?q=node", function(res) {
-  console.log("Got response: " + res.statusCode); 
-}).on('close', function() {
-  var d2 = new Date().getTime() - d1;
-  console.log("Delay : " + d2 + " ms");
-	});
+    console.log("Got response: " + res.statusCode); 
+  }).on('close', function() {
+    var d2 = new Date().getTime() - d1;
+    console.log("Delay : " + d2 + " ms");
+});
